9e55128cd691496e769ce729ab08630513fa5b62
Mark Anthony <markanthonylaredo@gmail.com>
2019-11-11 12:12:46 +0800

    Bug fixes and improvements

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
     pwa: {
         workboxPluginMode: "InjectManifest",
         workboxOptions: {
-            swSrc: "/service-worker.js"
+            swSrc: "src/service-worker.js"
         }
     }
 }
