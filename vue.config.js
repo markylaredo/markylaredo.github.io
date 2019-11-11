5fa54ada7b0ad6bdf6bae7a630f7ddbf95ddb66a
Mark Anthony <markanthonylaredo@gmail.com>
2019-11-11 12:36:22 +0800

    bug fix and improve injected manifest

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,8 @@
 module.exports = {
     pwa: {
+
+        appleMobileWebAppCapable:"yes",
+        manifestPath:"manifest.json",
         workboxPluginMode: "InjectManifest",
         workboxOptions: {
             swSrc: "src/service-worker.js"
