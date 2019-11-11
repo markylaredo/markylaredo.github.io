b25c73bc35da1c9cbf0d02694b2d514679dcd214
Mark Anthony <markanthonylaredo@gmail.com>
2019-11-11 18:56:27 +0800

    align center

--- src/components/ProfileHeader.vue
+++ src/components/ProfileHeader.vue
@@ -4,7 +4,7 @@
     height="400"
     :src="require('@/assets/keep_calm_and_code.jpg')"
   >
-      <v-layout class="text-center darken-3" justify-center>
+      <v-layout align-center class="text-center darken-3" justify-center>
         <v-flex md6 xs11 sm11>
           <h1 class="display-1 font-weight-thin mb-4" v-text="message"></h1>
           <h4 class="headline" v-text="description"></h4>
