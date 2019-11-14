6da9813e23e661b01fc5a5264f02501844e04204
Mark Anthony <markanthonylaredo@gmail.com>
2019-11-14 14:00:54 +0800

    Add my works

--- src/components/ProfileBodyDevWorks.vue
+++ src/components/ProfileBodyDevWorks.vue
@@ -1,17 +1,123 @@
 <template>
-  <v-parallax dark height="400" :src="require('@/assets/vs_code_background.jpg')">
-      <v-layout row justify-center class="pb-8">
-        <v-flex xs12 md12 sm12 class="text-center">
-          <div class="text-center">
+  <v-img :src="require('@/assets/vs_code_background.jpg')">
+    <v-container fluid>
+      <v-layout row justify-center>
+        <v-flex xs12 md12 sm12>
+          <div class="text-center white--text">
             <h3 class="my-8 headline">My Works</h3>
           </div>
         </v-flex>
 
-        <v-flex md3 xs9 sm9 class="text-center">
-          <h4 class="subtitle-2">
-            <v-icon small color="white">mdi-spin mdi-loading</v-icon>&nbsp;On Development...
-          </h4>
+        <v-flex xs12 md11 sm12>
+          <v-slide-group show-arrows :center-active="true" dark>
+            <v-slide-item
+              v-for="(work,i) in works"
+              :key="work+i"
+              v-slot:default="{ active, toggle }"
+            >
+              <v-card light class="ma-4" :elevation="8" width="300" @click="toggle">
+                <v-carousel
+                  delimiter-icon="mdi-minus"
+                  cycle
+                  hide-delimiter-background
+                  show-arrows-on-hover
+                  height="200"
+                  :interval="work.interval"
+                >
+                  <v-carousel-item v-for="img in work.images" :key="img">
+                    <v-img max-height="500" :src="require(`@/assets/myWorks/${img}`)" :alt="img">
+                      <template v-slot:placeholder>
+                        <v-row class="fill-height ma-0" align="center" justify="center">
+                          <v-progress-circular indeterminate color="light-blue accent-2 "></v-progress-circular>
+                        </v-row>
+                      </template>
+                    </v-img>
+                  </v-carousel-item>
+                </v-carousel>
+                <!-- <v-card-title v-text="work.system"></v-card-title> -->
+                <v-divider></v-divider>
+                <v-card-text>
+                  <h4 class="gray--text">System</h4>
+                  <p class="black--text text-wrap">{{work.description}}</p>
+                  <h4 class="gray--text">Platform</h4>
+                  <p class="black--text">{{work.platform}}</p>
+                  <h4 class="gray--text">Written in</h4>
+                  <p class="black--text">{{work.writtenIn}}</p>
+                </v-card-text>
+              </v-card>
+            </v-slide-item>
+          </v-slide-group>
         </v-flex>
       </v-layout>
-  </v-parallax>
+    </v-container>
+  </v-img>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      works: [
+        {
+          images: [
+            "irc_dashboard.png",
+            "irc_login.png",
+            "irc_student_Info.png"
+          ],
+          system: "IRC System",
+          description:
+            "Innovative Realtime Card System with SMS Notification and Parent Portal",
+          platform: "ASP.NET Web Forms",
+          writtenIn: "C#",
+          interval: Math.floor(Math.random() * (6000 - 5800) + 5000)
+        },
+        {
+          images: ["oms_login.jpg", "oms_dashboard.jpg", "oms_insights.jpg"],
+          system: "Loan Management System",
+          description: "OMS Multipurpose Cooperative Loan Management System",
+          platform: "VB.Net - Forms",
+          writtenIn: "Visual Basic .Net",
+          interval: Math.floor(Math.random() * (6000 - 5800) + 5000)
+        },
+        {
+          images: [
+            "stl_login.png",
+            "stl_dashboard.png",
+            "stl_employee_info.png"
+          ],
+          system: "HRP System",
+          description:
+            "Human Resource and Payroll System with Face Recognition",
+          platform: "ASP.NET MVC",
+          writtenIn: "C#",
+          interval: Math.floor(Math.random() * (6000 - 5800) + 5000)
+        },
+        {
+          images: [
+            "gorgo_landing_page.png",
+            "gorgo_registration.png",
+            "gorgo_talent_list.png",
+            "gorgo_talent_info.png"
+          ],
+          system: "GORGO",
+          description: "GORGO Talent Assistant",
+          platform: "ASP.NET MVC",
+          writtenIn: "C#",
+          interval: Math.floor(Math.random() * (6000 - 5800) + 5000)
+        },
+        {
+          images: [
+            "irc_bulk_sms_dashboard.jpg",
+            "irc_bulk_sms.jpg",
+            "irc_bulk_sms_log_stats.jpg"
+          ],
+          system: "IRC Bulk SMS",
+          description: "IRC Bulk SMS Announcement",
+          platform: "Windows Forms",
+          writtenIn: "C#",
+          interval: Math.floor(Math.random() * (6000 - 5800) + 5000)
+        }
+      ]
+    };
+  }
+};
+</script>
